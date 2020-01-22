e50de05e59ca8f2a82e9d31dcd9722c35417d893
catkira <benjamin@menkuec.de>
2020-01-22 21:11:39 +0100

    adapted to new log file format

--- analyze_calibration.m
+++ analyze_calibration.m
@@ -1,11 +1,17 @@
 clear
 
-data=importdata('putty.log');
-dac_codes = cell2mat(data.textdata(:,1));
+data=readcell('putty.log','FileType','text','Delimiter','\t');
+
+index = find(strcmp(data, 'RAMP BEGIN'));
+start_index = index(find(strcmp(data(index,2),'channel 1')))+2;
+end_index = find(strcmp(data(start_index:length(data)), 'RAMP END'));
+end_index = end_index(1) + start_index - 2;
+
+dac_codes = cell2mat(data(start_index:end_index,1));
 dac_codes = dac_codes(:,3:6);
 dac_codes = hex2dec(dac_codes);
 
-currents = data.data(:,1);
+currents = cell2mat(data(start_index:end_index,4))
 %%
 % ignoring first and last calibration value improves uncalibrated
 % linearity, this is probably the case because the DAC needs some head room
